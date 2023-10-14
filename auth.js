2349824f34f32bf21a999a55366a8c02c29ca3a9
Wookie3 <cbsbjb@gmail.com>
2023-10-14 14:55:15 -0400

    starting to add JWT

--- auth.js
+++ auth.js
@@ -1,3 +1,10 @@
+import jwt from 'jsonwebtoken'
+import express from 'express'
+// import students from './data.js'
+import dotenv from 'dotenv'
+dotenv.config();
+const router = express.Router()
+
 export const verifyPassword = (req, res, next) => {
     const password = req.body.Password
     const token = parseInt(req.headers['authorization'])
@@ -6,8 +13,16 @@ export const verifyPassword = (req, res, next) => {
         return next();
     }
     if (!(token === 12345)) {
-        return res.status(400).send('bad request (token)')
+        return res.status(401).send('unauthorized (token)')
     }
     console.log(req.method)
     next();
     }
+
+router.post('/login', (req, res) => {
+    const studentId = req.body.Student_ID
+    const token = jwt.sign({studentId}, precess.env.KEY, {expiresIn: '1m'})
+    return res.json({token})
+})
+
+export default router
--- index.js
+++ index.js
@@ -2,12 +2,14 @@ import express from 'express'
 import courseRouter from "./routes/courses.js"
 import studentRouter from "./routes/students.js"
 import enrollmentRouter from "./routes/enrollments.js"
-import { verifyPassword } from './auth.js'
+
+import authRouter, { verifyPassword } from './auth.js'
 
 const app = express()
 const PORT = 3000
 
 app.use(express.json())
+app.use('/login', authRouter)
 app.use('/', verifyPassword)
 app.use('/courses', courseRouter)
 app.use('/students', studentRouter)
